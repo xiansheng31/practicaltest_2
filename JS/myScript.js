a2ca94f7a38d573415c88f0d958a215713153c99
xiansheng31 <62184536+xiansheng31@users.noreply.github.com>
2020-11-13 16:32:46 +0800

    Add files via upload

---
+++ JS/myScript.js
@@ -0,0 +1,10 @@
+alert("You're about visit my E-resume but first...");
+
+var name = prompt("What is your name?");
+
+if (name.length != 0){
+	alert("Hello, " + name + ". Welcome to my E-resume!");  
+}
+else {
+  alert("Don't be shy!")
+}
